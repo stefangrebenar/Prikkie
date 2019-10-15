a717f112fbe0cbd65604a32cdd9a08b6be75065f
Benji3199 <benji3199@gmail.com>
2019-10-15 14:08:46 +0200

    Does it work now?

---
+++ app/src/main/java/com/example/prikkie/Api/RecipeApi/RecipeApi.java
@@ -0,0 +1,19 @@
+package com.example.prikkie.Api.RecipeApi;
+
+import android.content.Context;
+
+import com.android.volley.RequestQueue;
+
+import java.util.ArrayList;
+
+public abstract class RecipeApi {
+    protected Context context;
+    protected RequestQueue request;
+    protected ArrayList<Recipe> recipes = new ArrayList<Recipe>();
+
+    public abstract void getRecipeFromApi(String keywords, String includedIngredients, String excludedIngredients);
+
+    public ArrayList<Recipe> getRecipe() {
+        return recipes;
+    }
+}
