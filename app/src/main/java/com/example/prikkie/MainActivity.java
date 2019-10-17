35a7ee604f7c6348c578c9bb45dcc78bff7ddadd
stefangrebenar <34131744+stefangrebenar@users.noreply.github.com>
2019-10-17 11:24:28 +0200

    Revert "Working budget"

--- app/src/main/java/com/example/prikkie/MainActivity.java
+++ app/src/main/java/com/example/prikkie/MainActivity.java
@@ -8,39 +8,18 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.util.Log;
+
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText budgetID;
-    public static final String USER_PREF = "USER_PREF" ;
-    public static final String KEY_BUDGET = "KEY_BUDGET";
-    SharedPreferences sp;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        budgetID = (EditText)findViewById(R.id.budgetID);
-
-        sp = getSharedPreferences(USER_PREF, Context.MODE_PRIVATE);
-
-        if (sp.contains(KEY_BUDGET)) {
-            budgetID.setText(String.valueOf(sp.getInt(KEY_BUDGET, 0)));
-        }
-      
-       Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
@@ -52,19 +31,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    }
-
-
-    public void sendBudget(View view){
-
-
-        int budget = Integer.valueOf(budgetID.getText().toString());
-
-        SharedPreferences.Editor editor = sp.edit();
-        editor.putInt(KEY_BUDGET, budget);
-        editor.apply();
-
-        Toast.makeText(this, "Saved!", Toast.LENGTH_SHORT).show();
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
