7d609beecbeae7347de5deb0da9047eb5998a62f
Stefan Grebenar <stefangrebenar@hotmail.com>
2019-10-17 11:33:47 +0200

    Revert "Merge branch 'dev' into feature/budget"
    This reverts commit 5b3b8deaca6b3b99832c5ff15ffe888c5b62b9b1, reversing
    changes made to e52475a9410faf3bd1ffe0709642738b037be989.

--- app/src/main/java/com/example/prikkie/MainActivity.java
+++ app/src/main/java/com/example/prikkie/MainActivity.java
@@ -1,13 +1,7 @@
 package com.example.prikkie;
-import android.content.Intent;
-import android.os.Bundle;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -16,8 +10,6 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         budgetID = (EditText)findViewById(R.id.budgetID);
 
         sp = getSharedPreferences(USER_PREF, Context.MODE_PRIVATE);
@@ -39,19 +30,6 @@ public class MainActivity extends AppCompatActivity {
         if (sp.contains(KEY_BUDGET)) {
             budgetID.setText(String.valueOf(sp.getInt(KEY_BUDGET, 0)));
         }
-      
-       Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
     }
 
 
@@ -66,29 +44,6 @@ public class MainActivity extends AppCompatActivity {
 
         Toast.makeText(this, "Saved!", Toast.LENGTH_SHORT).show();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void onRecipeActivity(View v){
-        startActivity(new Intent(MainActivity.this, RecipeApiActivity.class));
     }
 }
