8ff330413118c7c674c1a7d896c033a04e947e23
Stefan Grebenar <stefangrebenar@hotmail.com>
2019-10-17 11:47:51 +0200

    Fix

--- app/src/main/java/com/example/prikkie/MainActivity.java
+++ app/src/main/java/com/example/prikkie/MainActivity.java
@@ -14,17 +14,17 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     EditText budgetID;
-    public static final String USER_PREF = "USER_PREF" ;
+    public static final String USER_PREF = "USER_PREF";
     public static final String KEY_BUDGET = "KEY_BUDGET";
     SharedPreferences sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        budgetID = (EditText)findViewById(R.id.budgetID);
-
+        budgetID = (EditText) findViewById(R.id.budgetID);
         sp = getSharedPreferences(USER_PREF, Context.MODE_PRIVATE);
 
         if (sp.contains(KEY_BUDGET)) {
@@ -32,18 +32,13 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
-    public void sendBudget(View view){
-
+    public void sendBudget(View view) {
 
         int budget = Integer.valueOf(budgetID.getText().toString());
-
         SharedPreferences.Editor editor = sp.edit();
         editor.putInt(KEY_BUDGET, budget);
         editor.apply();
 
         Toast.makeText(this, "Saved!", Toast.LENGTH_SHORT).show();
-
-
     }
 }
