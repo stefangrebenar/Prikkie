c75a145eac8829e09dfc34e62d89a74eb35083e2
Benji3199 <benji3199@gmail.com>
2019-11-05 12:10:18 +0100

    add user

---
+++ app/src/main/java/com/example/prikkie/user/User.java
@@ -0,0 +1,10 @@
+package com.example.prikkie.user;
+
+import com.example.prikkie.ingredients.IngredientClassificationComponent;
+
+import java.util.ArrayList;
+
+public class User {
+    public String Name;
+    public ArrayList<IngredientClassificationComponent> ingredientPreferences = new ArrayList<IngredientClassificationComponent>();
+}
