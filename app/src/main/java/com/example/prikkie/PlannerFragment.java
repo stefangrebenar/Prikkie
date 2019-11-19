c9b5f9f5424ca25fa52a33858320c999a3aca9f8
Benji3199 <benji3199@gmail.com>
2019-11-19 14:22:49 +0100

    set-up untill recipe-api and ingredient-api are ready

--- app/src/main/java/com/example/prikkie/PlannerFragment.java
+++ app/src/main/java/com/example/prikkie/PlannerFragment.java
@@ -1,5 +1,6 @@
 package com.example.prikkie;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,11 +10,64 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.prikkie.Api.IngredientApi.AHAPI;
+import com.example.prikkie.Api.recipe_api.Recipe;
+import com.example.prikkie.ingredientDB.Ingredient;
+
+import java.util.ArrayList;
+
 public class PlannerFragment extends Fragment {
+
+    private int budget;
+    private ArrayList<Recipe> recipes = new ArrayList<Recipe>();
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_planner,container,false);
+    }
+
+    public Recipe getRecipesByBudget(){
+        budget = 0; // get from shared preferences
+        int amountOfRecipes = 0; // get from api (Maybe without the excluded recipes)
+        int amountOfCheckedRecipes = 0;
+        int[] checkedRecipes = new int[amountOfRecipes];
+        Recipe finalRecipe = null;
+        ArrayList<Ingredient> excludedIngredients = new ArrayList<Ingredient>();
+        // Api get preferences?
+        do{
+            ArrayList<Recipe> recipes = getRandomRecipes(excludedIngredients, checkedRecipes);
+            for(Recipe recipe : recipes){
+                double recipePrice = getPriceForIngredients(recipe.ingredients);
+                if(recipePrice <= budget){
+                    finalRecipe = recipe;
+                    break;
+                }
+                checkedRecipes[amountOfCheckedRecipes] = recipe.id;
+                amountOfCheckedRecipes++;
+            }
+        }while(finalRecipe != null && amountOfCheckedRecipes < amountOfRecipes);
+
+        return finalRecipe;
+    }
+
+    public ArrayList<Recipe> getRandomRecipes(ArrayList<Ingredient> excludedIngredients, int[] checkedRecipes) {
+        ArrayList<Recipe> recipes = new ArrayList<Recipe>();
+
+        // Api call for recipes with preferences
+        // recipes = GetRandom(excludedIngredients, checkedRecipes);
+
+        return recipes;
+    }
+
+    private double getPriceForIngredients(ArrayList<Ingredient> ingredients){
+        double price = 0.0;
+        AHAPI api = new AHAPI();
+
+        for(Ingredient ingredient : ingredients){
+            price += api.getProducts(new Context(), ingredient.Dutch).get(0).price; // insert context fix
+        }
+
+        return price;
     }
 }
--- app/src/main/java/com/example/prikkie/api/recipe_api/Recipe.java
+++ app/src/main/java/com/example/prikkie/api/recipe_api/Recipe.java
@@ -1,10 +1,16 @@
 package com.example.prikkie.Api.recipe_api;
+import android.graphics.Bitmap;
+
 import java.util.ArrayList;
 
 public class Recipe {
+    public int id;
     public String title;
-    public String imagePath;
+    public String imagePath; // will probably be deprecated
+    public Bitmap bitmap; // probably the type of image
     public ArrayList<String> ingredients = new ArrayList<String>();
     public String discription;
-    public String href;//
+    public String method;
+    public int persons;
+    public String href; // will be deprecated
 }
