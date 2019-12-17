2e86c6f07845dd20982a5e416d623e27ef3f1408
Stefan Grebenar <stefangrebenar@hotmail.com>
2019-12-17 10:46:46 +0100

    leuk

--- app/src/main/java/com/example/prikkie/AccountFragment.java
+++ app/src/main/java/com/example/prikkie/AccountFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +17,9 @@ public class AccountFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_account,container,false);
+        return inflater.inflate(R.layout.fragment_account, container, false);
+
     }
+
+
 }
